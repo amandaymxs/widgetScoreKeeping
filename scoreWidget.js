ac0ee0bfbf84f1bf8dafd063e625829775386110
amandaymxs <amandaxinvestment@gmail.com>
2020-11-21 23:52:45 -0500

    toggle add points buttons

--- scoreWidget.js
+++ scoreWidget.js
@@ -3,7 +3,7 @@ let p2Score = document.querySelector("#p2Score");
 const p1AddPoint = document.querySelector("#p1AddPoint");
 const p2AddPoint = document.querySelector("#p2AddPoint");
 const winningScore = document.querySelector("#winningScore");
-
+const addPointButtons = document.querySelector("addPoint");
 
 //////////////////////////////////////////////////////////////
 
@@ -19,18 +19,36 @@ window.addEventListener("load", function () {
     winningScore.appendChild(docFragment);
 }, { once: true });
 
-
-
 /////////////////////////////////////////////////////////////
 
+
+//Note: winningScore.innerText is "5↵6↵7↵8↵9↵10↵11↵12" while winningScore.value is the selected string option.
 p1AddPoint.addEventListener('click', function () {
-    if (parseInt(p1Score.innerText) < parseInt(winningScore.innerText)) {
+    let winnningScoreValue = parseInt(winningScore.value);
+    if (parseInt(p1Score.innerText) < winnningScoreValue) {
         p1Score.innerText = parseInt(p1Score.innerText) + 1;
+        if (p1Score.innerText == winningScore.value) {
+            //add function to disable buttons - toggle
+            toggle(p1AddPoint);
+            toggle(p2AddPoint);
+        }
     }
+    console.dir(p1AddPoint);
 });
 
 p2AddPoint.addEventListener('click', function () {
-    if (parseInt(p2Score.innerText) < parseInt(winningScore.innerText)) {
+    let winnningScoreValue = parseInt(winningScore.value);
+    if (parseInt(p2Score.innerText) < parseInt(winningScore.value)) {
         p2Score.innerText = parseInt(p2Score.innerText) + 1;
+        if (p1Score.innerText == winningScore.value) {
+            toggle(p1AddPoint);
+            toggle(p2AddPoint);
+        }
     }
 });
+
+//////////////////////////////////////////////////////////////////
+
+function toggle(button) {
+    button.disabled = "OFF";
+}
