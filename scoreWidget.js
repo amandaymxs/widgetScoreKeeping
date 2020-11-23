5bc4e014b00c216f59f1df072f0be9726a978dde
amandaymxs <amandaxinvestment@gmail.com>
2020-11-22 20:56:44 -0500

    add Bulma template

--- scoreWidget.js
+++ scoreWidget.js
@@ -4,9 +4,10 @@ const p1AddPoint = document.querySelector("#p1AddPoint");
 const p2AddPoint = document.querySelector("#p2AddPoint");
 const winningScore = document.querySelector("#winningScore");
 const resetButton = document.querySelector(".resetButton");
+
 //////////////////////////////////////////////////////////////
 
-// winningScore.addEventListener('load', function () {
+// winningScoreSelect.addEventListener('load', function () {
 window.addEventListener("load", function () {
     let docFragment = document.createDocumentFragment();
     for (let i = 6; i <= 12; i++) {
@@ -18,46 +19,48 @@ window.addEventListener("load", function () {
     winningScore.appendChild(docFragment);
 }, { once: true });
 
-/////////////////////////////////////////////////////////////
+winningScore.addEventListener('change', function () {
+    reset();
+})
 
-
-//Note: winningScore.innerText is "5↵6↵7↵8↵9↵10↵11↵12" while winningScore.value is the selected string option.
+//Note: winningScoreSelectSelect.innerText is "5↵6↵7↵8↵9↵10↵11↵12" while winningScoreSelectSelect.value is the selected string option.
 p1AddPoint.addEventListener('click', function () {
     let winnningScoreValue = parseInt(winningScore.value);
     if (parseInt(p1Score.innerText) < winnningScoreValue) {
         p1Score.innerText = parseInt(p1Score.innerText) + 1;
-        if (p1Score.innerText == winningScore.value) {
+        if (p1Score.innerText == winnningScoreValue) {
             //add function to disable buttons - toggle
-            toggle(p1AddPoint);
-            toggle(p2AddPoint);
+            isGameOver(p1AddPoint);
+            isGameOver(p2AddPoint);
         }
     }
-    console.dir(p1AddPoint);
 });
 
 p2AddPoint.addEventListener('click', function () {
     let winnningScoreValue = parseInt(winningScore.value);
-    if (parseInt(p2Score.innerText) < parseInt(winningScore.value)) {
+    if (parseInt(p2Score.innerText) < parseInt(winnningScoreValue)) {
         p2Score.innerText = parseInt(p2Score.innerText) + 1;
-        if (p1Score.innerText == winningScore.value) {
-            toggle(p1AddPoint);
-            toggle(p2AddPoint);
+        if (p2Score.innerText == winnningScoreValue) {
+            isGameOver(p1AddPoint);
+            isGameOver(p2AddPoint);
         }
     }
 });
 
-console.dir(resetButton);
-resetButton.addEventListener('click', function () {
+resetButton.addEventListener('click', reset);
+
+//////////////////////////////////////////////////////////////////////
+
+function reset() {
     p1Score.innerText = "0";
     p2Score.innerText = "0";
-    if (p1AddPoint.disabled == true) {
-        toggle(p1AddPoint);
-        toggle(p2AddPoint);
+    if (p1AddPoint.disabled) {
+        isGameOver(p1AddPoint);
+        isGameOver(p2AddPoint);
     }
-})
-//////////////////////////////////////////////////////////////////
+};
 
-function toggle(button) {
+function isGameOver(button) {
     switch (button.disabled) {
         case true:
             button.disabled = false;
